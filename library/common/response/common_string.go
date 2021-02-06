23474c40112209a57eeca6bc312fdc3ccdb5e44a
ChenChienhang <jankins.chen@foxmail.com>
2021-02-06 12:37:51 +0800

    feature: 字符串常量

--- library/common/response/common_string.go
+++ library/common/response/common_string.go
@@ -4,10 +4,7 @@
 package response
 
 var (
-	VerificationCodeEmailBody = `	Hi! 亲爱的朋友:
-感谢您使用实验教学系统。为保证用户正常使用我们的系统，请您使用下方的验证码用以验证以帮助我们确认您的电子邮件地址有效：VerificationCode
-	祝您使用愉快！
-	此邮件由系统自动发送，
-	请勿回复。
-	TIMENOW`
+	VerificationCodeEmailBody = "	Hi! 亲爱的朋友:\n" +
+		"感谢您使用实验教学系统。为保证用户正常使用我们的系统，请您使用下方的验证码用以验证以帮助我们确认您的电子邮件地址有效：\n%s\n" +
+		"祝您使用愉快！\n此邮件由系统自动发送，请勿回复。\n%s"
 )
