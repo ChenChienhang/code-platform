378816ba31f3998e0bc483f2970c94025c20ecfb
ChenChienhang <jankins.chen@foxmail.com>
2021-03-26 21:37:06 +0800

    fix: 修复bug

--- app/service/comment.go
+++ app/service/comment.go
@@ -6,11 +6,10 @@ package service
 import (
 	"code-platform/app/dao"
 	"code-platform/app/model"
-	"code-platform/library/common/code"
 	"code-platform/library/common/response"
+	"github.com/gogf/gf/container/gset"
 	"github.com/gogf/gf/database/gdb"
 	"github.com/gogf/gf/frame/g"
-	"github.com/gogf/gf/util/gconv"
 	"math"
 )
 
@@ -18,30 +17,83 @@ var CourseCommentService = new(courseCommentService)
 
 type courseCommentService struct{}
 
-func (s *courseCommentService) ListCourseCommentByCourseId(req *model.ListCourseCommentReq) (*response.PageResp, error) {
-	var CommentResps = make([]*model.CourseCommentEntity, 0)
+// ListCourseCommentByCourseId 列出所有课程评论
+// @receiver s
+// @params req
+// @return *response.PageResp
+// @return error
+// @date 2021-03-16 14:25:54
+func (s *courseCommentService) ListCourseCommentByCourseId(req *model.ListCourseCommentReq) (resp *response.PageResp, err error) {
+	var records = make([]*model.CourseCommentEntity, 0)
 	// 分页，排序
-	if err := dao.CourseComment.Page(req.PageCurrent, req.PageSize).Order(dao.CourseComment.Columns.CreatedAt+" desc").
+	if err = dao.CourseComment.Page(req.PageCurrent, req.PageSize).Order(dao.CourseComment.Columns.CreatedAt+" desc").
 		// 主评，对应课程
 		Where(dao.CourseComment.Columns.Pid, 0).And(dao.CourseComment.Columns.CourseId, req.CourseId).
 		FieldsEx(dao.CourseComment.Columns.DeletedAt).
-		ScanList(&CommentResps, "Comment"); err != nil {
+		ScanList(&records, "Comment"); err != nil {
 		return nil, err
 	}
 
 	// 查子评论
-	if err := dao.CourseComment.
+	if err = dao.CourseComment.
 		Where(dao.CourseComment.Columns.Pid,
-			gdb.ListItemValuesUnique(CommentResps, "Comment", "CourseCommentId")).
+			gdb.ListItemValuesUnique(records, "Comment", "CourseCommentId")).
 		Order(dao.CourseComment.Columns.CreatedAt+" desc").
 		FieldsEx(dao.CourseComment.Columns.DeletedAt).
-		ScanList(&CommentResps, "ReplyComments", "Comment", "pid:CourseCommentId"); err != nil {
+		ScanList(&records, "ReplyComments", "Comment", "pid:CourseCommentId"); err != nil {
 		return nil, err
 	}
 
-	for _, v := range CommentResps {
+	for _, v := range records {
 		if v.ReplyComments == nil {
 			v.ReplyComments = make([]*model.CourseCommentResp, 0)
+		}
+	}
+
+	// 收集userId
+	userIds := gset.NewIntSet(false)
+	for _, v := range records {
+		userIds.Add(v.Comment.UserId)
+		for _, v1 := range v.ReplyComments {
+			userIds.Add(v1.UserId)
+			userIds.Add(v1.ReplyUserId)
+		}
+	}
+
+	userDetails := make([]*model.SysUser, 0)
+	if err = dao.SysUser.WherePri(userIds.Slice()).Fields(
+		dao.SysUser.Columns.UserId,
+		dao.SysUser.Columns.RealName,
+		dao.SysUser.Columns.AvatarUrl,
+	).FindScan(&userDetails); err != nil {
+		return nil, err
+	}
+	// 装配
+	for _, v := range records {
+		for _, u := range userDetails {
+			if v.Comment.UserId == u.UserId {
+				v.Comment.Username = u.RealName
+				v.Comment.UserAvatarUrl = u.AvatarUrl
+			}
+		}
+		for _, v1 := range v.ReplyComments {
+			count := 0
+			for _, u := range userDetails {
+				if v1.UserId == u.UserId {
+					v1.Username = u.RealName
+					count++
+					if count == 2 {
+						break
+					}
+				}
+				if v1.ReplyUserId == u.UserId {
+					v1.ReplyUsername = u.RealName
+					count++
+					if count == 2 {
+						break
+					}
+				}
+			}
 		}
 	}
 
@@ -54,10 +106,10 @@ func (s *courseCommentService) ListCourseCommentByCourseId(req *model.ListCourse
 	}
 
 	// 分页信息整合
-	resp := &response.PageResp{
-		Records: CommentResps,
+	resp = &response.PageResp{
+		Records: records,
 		PageInfo: &response.PageInfo{
-			Size:    len(CommentResps),
+			Size:    len(records),
 			Total:   count,
 			Current: req.PageCurrent,
 			Pages:   int(math.Ceil(float64(count) / float64(req.PageSize))),
@@ -65,28 +117,82 @@ func (s *courseCommentService) ListCourseCommentByCourseId(req *model.ListCourse
 	return resp, nil
 }
 
-func (s *courseCommentService) ListLabCommentByLabId(req *model.ListLabCommentReq) (*response.PageResp, error) {
-	var comments = make([]*model.LabCommentEntity, 0)
+// ListLabCommentByLabId 列出所有实验评论
+// @receiver s
+// @params req
+// @return *response.PageResp
+// @return error
+// @date 2021-03-16 14:26:05
+func (s *courseCommentService) ListLabCommentByLabId(req *model.ListLabCommentReq) (resp *response.PageResp, err error) {
+	var records = make([]*model.LabCommentEntity, 0)
 	// 分页，排序
-	if err := dao.LabComment.Page(req.PageCurrent, req.PageSize).Order(dao.LabComment.Columns.CreatedAt+" desc").
+	if err = dao.LabComment.Page(req.PageCurrent, req.PageSize).Order(dao.LabComment.Columns.CreatedAt+" desc").
 		// 主评，对应课程
 		Where(dao.LabComment.Columns.Pid, 0).And(dao.LabComment.Columns.LabId, req.LabId).
 		FieldsEx(dao.LabComment.Columns.DeletedAt).
-		ScanList(&comments, "Comment"); err != nil {
+		ScanList(&records, "Comment"); err != nil {
 		return nil, err
 	}
 
 	// 查子评论
-	if err := dao.LabComment.
+	if err = dao.LabComment.
 		Where(dao.LabComment.Columns.Pid,
-			gdb.ListItemValuesUnique(comments, "Comment", "CourseCommentId")).
+			gdb.ListItemValuesUnique(records, "Comment", "CourseCommentId")).
 		FieldsEx(dao.LabComment.Columns.DeletedAt).
-		ScanList(&comments, "ReplyComments", "Comment", "pid:CourseCommentId"); err != nil {
+		ScanList(&records, "ReplyComments", "Comment", "pid:CourseCommentId"); err != nil {
 		return nil, err
 	}
-	for _, v := range comments {
+	for _, v := range records {
 		if v.ReplyComments == nil {
 			v.ReplyComments = make([]*model.LabCommentResp, 0)
+		}
+	}
+
+	// 收集userId
+	userIds := gset.NewIntSet(false)
+	for _, v := range records {
+		userIds.Add(v.Comment.UserId)
+		for _, v1 := range v.ReplyComments {
+			userIds.Add(v1.UserId)
+			userIds.Add(v1.ReplyUserId)
+		}
+	}
+
+	// 装配字段
+	userDetails := make([]*model.SysUser, 0)
+	if err = dao.SysUser.WherePri(userIds.Slice()).Fields(
+		dao.SysUser.Columns.AvatarUrl,
+		dao.SysUser.Columns.UserId,
+		dao.SysUser.Columns.RealName,
+	).FindScan(&userDetails); err != nil {
+		return nil, err
+	}
+	for _, v := range records {
+		for _, u := range userDetails {
+			if v.Comment.UserId == u.UserId {
+				v.Comment.Username = u.RealName
+				v.Comment.UserAvatarUrl = u.AvatarUrl
+			}
+		}
+		for _, v1 := range v.ReplyComments {
+			count := 0
+			for _, u := range userDetails {
+				if v1.UserId == u.UserId {
+					v1.Username = u.RealName
+					v1.UserAvatarUrl = u.AvatarUrl
+					count++
+					if count == 2 {
+						break
+					}
+				}
+				if v1.ReplyUserId == u.UserId {
+					v1.ReplyUsername = u.RealName
+					count++
+					if count == 2 {
+						break
+					}
+				}
+			}
 		}
 	}
 
@@ -99,10 +205,10 @@ func (s *courseCommentService) ListLabCommentByLabId(req *model.ListLabCommentRe
 	}
 
 	// 分页信息整合
-	resp := &response.PageResp{
-		Records: comments,
+	resp = &response.PageResp{
+		Records: records,
 		PageInfo: &response.PageInfo{
-			Size:    len(comments),
+			Size:    len(records),
 			Total:   count,
 			Current: req.PageCurrent,
 			Pages:   int(math.Ceil(float64(count) / float64(req.PageSize))),
@@ -117,43 +223,31 @@ func (s *courseCommentService) ListLabCommentByLabId(req *model.ListLabCommentRe
 // @date 2021-01-31 18:29:13
 func (s *courseCommentService) InsertCourseComment(req *model.InsertCourseCommentReq) (err error) {
 	// 保存模型
-	var courseComment *model.CourseComment
-	if err = gconv.Struct(req, &courseComment); err != nil {
-		return err
+	var courseComment = &model.CourseComment{
+		CommentText: req.CommentText,
+		UserId:      req.UserId,
 	}
-	if courseComment.ReplyId != 0 {
+	if req.ReplyId != 0 {
 		// 回复别人的评论,要做处理
-		tmp, err := dao.CourseComment.WherePri(courseComment.ReplyId).
-			Fields(dao.CourseComment.Columns.Pid, dao.CourseComment.Columns.Username).FindOne()
+		tmp, err := dao.CourseComment.WherePri(req.ReplyId).
+			Fields(dao.CourseComment.Columns.Pid, dao.CourseComment.Columns.ReplyUserId).FindOne()
 		if err != nil {
 			return err
 		}
 		// 回复的是主评,则pid为主评id
 		if tmp.Pid == 0 {
-			courseComment.Pid = courseComment.ReplyId
+			courseComment.Pid = req.ReplyId
 		} else {
 			// 否则，pid与被回复的pid保持一致
 			courseComment.Pid = tmp.Pid
 		}
 
-		// 被回复的那条评论的用户昵称
-		courseComment.ReplyUsername = tmp.Username
-	}
-	// 回复评论的用户昵称
-	if one, err := dao.SysUser.WherePri(courseComment.UserId).
-		Fields(dao.SysUser.Columns.NickName,
-			dao.SysUser.Columns.AvatarUrl).
-		FindOne(); err != nil {
-		return err
-	} else if one == nil {
-		return code.UserNotExistError
-	} else {
-		courseComment.Username = one.NickName
-		courseComment.UserAvatarUrl = one.AvatarUrl
+		// 被回复的那条评论的用户id
+		courseComment.ReplyUserId = tmp.ReplyUserId
 	}
 
 	// 保存
-	if _, err = dao.CourseComment.Insert(courseComment); err != nil {
+	if _, err = dao.CourseComment.Data(courseComment).Insert(); err != nil {
 		return err
 	}
 	return nil
@@ -165,39 +259,28 @@ func (s *courseCommentService) InsertCourseComment(req *model.InsertCourseCommen
 // @return error
 // @date 2021-01-31 18:29:22
 func (s *courseCommentService) InsertLabComment(req *model.InsertLabCommentReq) (err error) {
-	var labComment *model.LabComment
-	if err = gconv.Struct(req, &labComment); err != nil {
-		return err
+	var labComment = &model.LabComment{
+		CommentText: req.CommentText,
+		UserId:      req.UserId,
 	}
-	if labComment.ReplyId != 0 {
+	if req.ReplyId != 0 {
 		// 回复别人的评论,要做处理
-		replyComment, err := dao.LabComment.WherePri(labComment.ReplyId).
-			Fields(dao.LabComment.Columns.Pid, dao.LabComment.Columns.Username).FindOne()
+		replyComment, err := dao.LabComment.WherePri(req.ReplyId).
+			Fields(dao.LabComment.Columns.Pid, dao.LabComment.Columns.ReplyUserId).FindOne()
 		if err != nil {
 			return err
 		}
 		// 回复的是主评,则pid为主评id
 		if replyComment.Pid == 0 {
-			labComment.Pid = labComment.ReplyId
+			labComment.Pid = req.ReplyId
 		} else {
 			// 否则，pid与被回复的pid保持一致
 			labComment.Pid = replyComment.Pid
 		}
 		// 被回复的那条评论的用户昵称
-		labComment.ReplyUsername = replyComment.Username
+		labComment.ReplyUserId = replyComment.ReplyUserId
 	}
-	// 回复评论的用户昵称
-	if one, err := dao.SysUser.WherePri(labComment.UserId).
-		Fields(dao.SysUser.Columns.NickName,
-			dao.SysUser.Columns.AvatarUrl).
-		FindOne(); err != nil {
-		return err
-	} else if one == nil {
-		return code.UserNotExistError
-	} else {
-		labComment.Username = one.NickName
-		labComment.UserAvatarUrl = one.AvatarUrl
-	}
+
 	// 保存
 	if _, err = dao.LabComment.Insert(labComment); err != nil {
 		return err
@@ -205,21 +288,37 @@ func (s *courseCommentService) InsertLabComment(req *model.InsertLabCommentReq) 
 	return nil
 }
 
+// DeleteLabComment 删除实验评论
+// @receiver s
+// @params commentId
+// @params userId
+// @return err
+// @date 2021-03-16 14:26:23
 func (s *courseCommentService) DeleteLabComment(commentId int, userId int) (err error) {
-	if _, err = dao.CourseComment.Where(g.Map{
-		dao.LabComment.Columns.LabId:  commentId,
-		dao.LabComment.Columns.UserId: userId,
-	}).Save(dao.LabComment.Columns.CommentText, "该评论已删除"); err != nil {
+	if _, err = dao.LabComment.Where(g.Map{
+		dao.LabComment.Columns.LabCommentId: commentId,
+		dao.LabComment.Columns.UserId:       userId,
+	}).Data(g.Map{
+		dao.LabComment.Columns.CommentText: "该评论已删除",
+	}).Update(); err != nil {
 		return err
 	}
 	return nil
 }
 
+// DeleteCourseComment 删除课程评论
+// @receiver s
+// @params commentId
+// @params userId
+// @return err
+// @date 2021-03-16 14:26:32
 func (s *courseCommentService) DeleteCourseComment(commentId int, userId int) (err error) {
 	if _, err = dao.CourseComment.Where(g.Map{
-		dao.CourseComment.Columns.CourseId: commentId,
-		dao.CourseComment.Columns.UserId:   userId,
-	}).Save(dao.CourseComment.Columns.CommentText, "该评论已删除"); err != nil {
+		dao.CourseComment.Columns.CourseCommentId: commentId,
+		dao.CourseComment.Columns.UserId:          userId,
+	}).Data(g.Map{
+		dao.CourseComment.Columns.CommentText: "该评论已删除",
+	}).Update(); err != nil {
 		return err
 	}
 	return nil
